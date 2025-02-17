3c34a490e5a01797806ecb918a2de835dee05fe2
Maxim Vasilyev <mayxis@inbox.ru>
2025-02-18 00:00:12 +0300

    fix: router

--- internal/router/router.go
+++ internal/router/router.go
@@ -5,6 +5,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/mdvasilyev/avito-shop/internal/handler"
 	"github.com/mdvasilyev/avito-shop/internal/middleware"
+	"github.com/mdvasilyev/avito-shop/internal/repository"
+	"github.com/mdvasilyev/avito-shop/internal/service"
 	"log/slog"
 )
 
@@ -13,13 +15,13 @@ func SetupRoutes(lgr *slog.Logger, rtr *gin.Engine, dbConn *sql.DB) {
 
 	apiGroup := rtr.Group("/api")
 
-	authService := handler.NewAuthService(dbConn)
-	merchService := handler.NewMerchService(dbConn)
-	userService := handler.NewUserService(dbConn)
-	coinService := handler.NewCoinService(dbConn)
+	authHandler := handler.NewAuthHandler(lgr, service.NewAuthService(lgr, repository.NewAuthRepository(lgr, dbConn)))
+	merchHandler := handler.NewMerchHandler(lgr, service.NewMerchService(lgr, repository.NewMerchRepository(lgr, dbConn)))
+	userHandler := handler.NewUserHandler(lgr, service.NewUserService(lgr, repository.NewUserRepository(lgr, dbConn)))
+	coinHandler := handler.NewCoinHandler(lgr, service.NewCoinService(lgr, repository.NewCoinRepository(lgr, dbConn)))
 
-	apiGroup.POST("/auth", authService.Auth)
-	apiGroup.POST("/sendCoin", middleware.AuthMiddleware(lgr), coinService.SendCoin)
-	apiGroup.GET("/info", middleware.AuthMiddleware(lgr), userService.Info)
-	apiGroup.GET("/buy/:item", middleware.AuthMiddleware(lgr), merchService.BuyItem)
+	apiGroup.POST("/auth", authHandler.Auth)
+	apiGroup.POST("/sendCoin", middleware.AuthMiddleware(lgr), coinHandler.SendCoin)
+	apiGroup.GET("/info", middleware.AuthMiddleware(lgr), userHandler.Info)
+	apiGroup.GET("/buy/:item", middleware.AuthMiddleware(lgr), merchHandler.BuyItem)
 }
